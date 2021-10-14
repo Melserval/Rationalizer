1f8a942d667d9c9ce64c5e8e35287f79dfd26bf3
Serhii Melnykov <serhii.melnykov@outlook.com>
2021-10-15 00:07:11 +0300

    сверстана (предварительная версия) форма добавление товара

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 "use strict";
 
+const formItemCreate = document.getElementById("form-create-item");
+const btnItemCreateApply = document.getElementById("item-create-apply");
+const btnItemCreateDone = document.getElementById("item-create-done");
 
-function main(hello, goddbye) {
-    return 1;
-}
+btnItemCreateDone.addEventListener("click", function (e) {
+    formItemCreate.parentElement.style.height = 0;
+});
