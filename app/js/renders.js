5585b3d591b7fb28974708bed9555ecbe4f5b3e5
Serhii Melnykov <serhii.melnykov@outlook.com>
2021-10-31 22:55:41 +0200

    добавлены юниты ListUnit и OrderList и рендеры для них

--- app/js/renders.js
+++ app/js/renders.js
@@ -1,7 +1,10 @@
 // элементы ui.
 "use strict";
 
-class RenderAssortimentUnit  {
+/**
+ * Представляет элемент ассортимента в списках товаров/заказов.
+ */
+class RenderListUnit  {
 	_node_li = document.createElement('li');
 	_span_name = document.createElement('span');
 	_span_amount = document.createElement('span');
@@ -24,3 +27,40 @@ class RenderAssortimentUnit  {
 	set setAmount(value) { this._span_amount.textContent = value; }
 	set setPrice(value) { this._span_price.textContent = value; }
 }; 
+
+/**
+ * Представление списка элементов ассортимента.
+ */
+class RenderOrderList {
+	_node_div = document.createElement("div");
+	_header  = document.createElement('header');
+	_ul = document.createElement('ul');
+	_span_label = document.createElement('span');
+	_span_term = document.createElement('span');
+	_span_quantity = document.createElement('span');
+	_span_total = document.createElement('span');
+
+	constructor() { 
+		this._header.append(
+			this._span_label,
+			this._span_term,
+			this._span_quantity,
+			this._span_total
+		);
+		this._node_div.append(this._header);
+		this._node_div.append(this._ul);
+		this._ul.classList.add("items-list")
+		this._header.classList.add("items-list-header")
+		this._node_div.classList.add("block-order-list");
+	}
+	insertInto(nodeElement) {
+		nodeElement.append(this._node_div);
+	}
+	remove() {
+		this._node_li.remove();
+	}
+	set label(label) { this._span_label.textContent = label; }
+	set term(term) { this._span_term.textContent = term; }
+	set quantity(quantity) { this._span_quantity.textContent = quantity; }
+	set total(total) { this._span_total.textContent = total; }
+ }
--- app/js/units.js
+++ app/js/units.js
@@ -1,27 +1,70 @@
 'use strict';
 
 /**
- * Единица ассортимента. Содержит всю основную информацию.
+ * Главная Единица Ассортимента. Содержит всю основную информацию.
  * Остальные классы, представляющие данные по ассортименту,
- * должны ссылаться на этот класс и хранить только ссылку,
+ * должны ссылаться на их экзэмляры и хранить только ссылку,
  * количество, стоимость и дату приобритения с примечанием.
- * 
- * @param {string} name название единицы ассортимента
- * @param {number} amount количество
- * @param {number} price стоимость
- * @param {Symbol} vendor вид распостранения
- * @param {Symbol} measure в каких единицах измерятся
- * @param {string} [category] категория 
- * @param {string} [desc] описание 
- * @param {string} [unitid] идентификатор
  */
-function AssortimentUnit(name, amount, price, vendor, measure, category, desc, unitid) {
-	this.name = name;
-	this.amount = amount;
-	this.price = price;
-	this.packageType = vendor;
-	this.measureType = measure;
-	this.category = category || "общая";
-	this.describe = desc || "";
-	this.id = unitid || Date.now().toString(24);
+class AssortimentUnit {
+	/**
+	 * @param {string} title название единицы ассортимента
+	 * @param {number} amount количество
+	 * @param {number} price стоимость
+	 * @param {Symbol} vendor вид распостранения
+	 * @param {Symbol} measure в каких единицах измерятся
+	 * @param {string} [category] категория 
+	 * @param {string} [desc] описание 
+	 * @param {string} [unitid] идентификатор
+	 */
+	constructor(title, amount, price, vendor, measure, category, desc, unitid) {
+		this.title = title;
+		this.amount = amount;
+		this.price = price;
+		this.packageType = vendor;
+		this.measureType = measure;
+		this.category = category || "общая";
+		this.describe = desc || "";
+		this.id = unitid || Date.now().toString(24);
+	}
+}
+
+/**
+ * Объект - элемент для списка ассортимента.
+ * предоставляет передачу данных для рендера
+ * и хранит "свои" значения для цены/количества.
+ */
+class ListUnit {
+	/**
+	 * @param {AssortimentUnit} unit
+	 */
+	constructor(unit) {
+		this.link = unit;
+	}
+	get title() { return this.link.title; }
+	get price() { return this.link.price; }
+	get amount() { return this.link.amount; }
+}
+
+/**
+ * Список единиц ассортимента.
+ * 
+ * Эти списки не должны хранить собственные (полные) экземпляры ассортимента,
+ * а только ссылку(и) на основное хранилище ассортимента и связанные данные,
+ * (количества, цены, примечания) с различными идентификаторами для фильтрации.
+ * Они не вызывают методы управления коллекцией обьекта-хранилища данных, но
+ * передают ему особый объект с директивами.
+ */
+class OrderList {
+	/**
+	 * @param {string} listName название-идентификатор списка.
+	*/
+	constructor(listname) {
+		this.listName = listname || "список ассортимента";
+		this.listUnitCollection = [];
+	}
+
+	add(unit) {
+		this.listUnitCollection.push(unit);
+	}
 }
