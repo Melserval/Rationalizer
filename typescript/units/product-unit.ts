6204024add8783975f261764e728918cbd4f5169
Serhii Melnykov <serhii.melnykov@outlook.com>
2022-02-07 21:11:54 +0200

    TS-адаптация и переработка product-unit.ts. добавлен тип для json версии ProductUnit.

--- typescript/units/product-unit.ts
+++ typescript/units/product-unit.ts
@@ -4,42 +4,43 @@
  * Остальные классы, представляющие данные по ассортименту, должны
  * ТОЛЬКО извлекать из них информацию. Они не должны дублироваться.
  */
-export default class ProductUnit {
-	/**
-	 * @param {string} title название единицы ассортимента
-	 * @param {number} amount количество
-	 * @param {number} price стоимость
-	 * @param {Symbol} vendor вид распостранения
-	 * @param {Symbol} measure в каких единицах измерятся
-	 * @param {string} [category] категория 
-	 * @param {string} [desc] описание 
-	 * @param {string} [unitid] идентификатор
-	 */
-	constructor(title, amount, price, vendor, measure, category, desc, unitid) {
-		this.title = title;
-		this.amount = amount;
-		this.price = price;
-		this.packageType = vendor;
-		this.measureType = measure;
-		this.category = category || "общая";
-		this.describe = desc || "";
-		this.id = unitid || Date.now().toString(24);
-	}
+class ProductUnit {
 
-	toJSON() {
+	/** 
+	 * @param title название единицы ассортимента
+	 * @param amount количество
+	 * @param price стоимость
+	 * @param packageType вид распостранения
+	 * @param measureType в каких единицах измерятся
+	 * @param category категория 
+	 * @param describe описание
+	 * @param id идентификатор
+	 */
+	constructor(
+		public title: string, 
+		public amount: number, 
+		public price: number, 
+		public packageType: symbol, 
+		public measureType: symbol, 
+		public category: string = "общая",
+		public describe: string = "", 
+		public id: string = Date.now().toString(24)
+	) { }
+
+	toJSON(): ProductUnitJson {
 		return {
 			"title": this.title,
 		 	"amount": this.amount,
 			"price": this.price,
-		 	"packageType": Symbol.keyFor(this.packageType),
-		 	"measureType": Symbol.keyFor(this.measureType),
+		 	"packageType": Symbol.keyFor(this.packageType) ?? "UNKNOW", //TODO: Разобраться с допустимыми строковыми значениями для типов учетных единиц.
+		 	"measureType": Symbol.keyFor(this.measureType) ?? "UNKNOW",
 		 	"category": this.category,
 		 	"describe": this.describe,
 		 	"id": this.id
 		};
 	}
 
-	static fromJson(json) {
+	static fromJson(json: ProductUnitJson) {
 		return new this(json.title ?? null, 
 						json.amount ?? null, 
 						json.price ?? null, 
@@ -51,3 +52,16 @@ export default class ProductUnit {
 	}
 	// TODO: реализация механизма сохранения обновления данных обьекта в хранилищах данных.
 }
+
+type ProductUnitJson = {
+	title: string;
+	amount: number;
+	price: number;
+	packageType: string;
+	measureType: string;
+	category: string;
+	describe: string;
+	id: string;
+}
+
+export {ProductUnit, ProductUnitJson};
