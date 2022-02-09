b705ab53e9f0f324f9c70ee84277aaf428b2359a
Serhii Melnykov <serhii.melnykov@outlook.com>
2022-02-09 17:54:53 +0200

    TS-адаптация form-create-order-list.ts

--- typescript/form-create-order-list.ts
+++ typescript/form-create-order-list.ts
@@ -5,46 +5,50 @@
  * Назначение обработчиков вызываемых при создании списка
  * с помощью интерфейса формы.
  */
-export default function createdOrderList(callback) {
+export default function createdOrderList(callback: CallableFunction) {
 	callbacks.push(callback);
 }
 
-const callbacks = [];
+const callbacks = new Array<CallableFunction>();
 
 // --- создание списка на выборочное количество дней ---
-const btn_CustomDaysList = document.getElementById("btn_custom_days_list");
+const btn_CustomDaysList = document.getElementById("btn_custom_days_list") as HTMLButtonElement;
 
 btn_CustomDaysList.addEventListener("click", function (e) {
 	console.dir(e);
-    if (e.target != this) return;
-    createOrderList(e.target.firstElementChild.value);
+    const element = e.target as HTMLElement;
+    if (element != this) return;
+    createOrderList((element.firstElementChild as HTMLInputElement).value);
 });
 
 btn_CustomDaysList.addEventListener("focus", function (e) {
-    console.dir(e);
-    if (e.target !== this) return;
-    e.target.firstElementChild.focus();
+    const element = e.target as HTMLButtonElement;
+    if (element !== this) return;
+        (element.firstElementChild as HTMLInputElement).focus();
 });
 
 btn_CustomDaysList.addEventListener("keypress", function (e) {
-    if (e.target.nodeName !== "INPUT" || e.code !== "NumpadEnter") return;
-    console.log(e.target.value);
+    const element = e.target as HTMLElement;
+    if (element.nodeName !== "INPUT" || e.code !== "NumpadEnter") return;
+    console.log((element as HTMLInputElement).value);
 });
 
+// TODO: Разобраться с возможным null значением ссылок на элементы. Проверить как будет работать если вернуть null.
+
 // --- создание списка на 1 день ---
-const btn_DayList  = document.getElementById("btn_day_list");
+const btn_DayList  = document.getElementById("btn_day_list") as HTMLButtonElement;
 btn_DayList.addEventListener("click", () => createOrderList(1));
 
 // --- создание списка на неделю (7 дней) ---
-const btn_WeakList = document.getElementById("btn_weak_list");
+const btn_WeakList = document.getElementById("btn_weak_list") as HTMLButtonElement;
 btn_WeakList.addEventListener("click", () => createOrderList(7));
 
 // --- создание списка на месяц (30 дней) ---
-const btn_MonthList = document.getElementById("btn_month_list");
+const btn_MonthList = document.getElementById("btn_month_list") as HTMLButtonElement;
 btn_MonthList.addEventListener("click", () => createOrderList(30));
 
 // --- создание отдельного списка покупок ---
-const btn_OrderList = document.getElementById("btn_order_list");
+const btn_OrderList = document.getElementById("btn_order_list") as HTMLButtonElement;
 btn_OrderList.addEventListener("click", () => createOrderList(0));
 
-const createOrderList = (arg) => callbacks.forEach(callback => callback(arg));
+const createOrderList = (arg: any) => callbacks.forEach(callback => callback(arg));
