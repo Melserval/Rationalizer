26a9afecd5c2c9f71e4012f52d3351afb1d435b4
Serhii Melnykov <serhii.melnykov@outlook.com>
2022-06-07 01:26:02 +0300

    реализована загрузка товаров из базы данных.

--- src/typescript/datastorage.ts
+++ src/typescript/datastorage.ts
@@ -60,11 +60,6 @@ export const removeProductUnit = function(
 	if (callback) setDataResult.then(callback.bind(null, null), callback);
 };
 
-// симуляция получения данных по сети...
-const loaddata = new Promise(function (resolve, reject) {
-	
-});
-
 const localstorDataSet = {
 	LOCAL_STORAGE_KEY: 'product_unit_set',
 	getData() {
@@ -97,24 +92,73 @@ const localstorDataSet = {
 	}
 };
 
-const constDataSet = Promise.resolve<ProductUnit[]>(new Array<[string, number, number, uType.VendorType, uType.MeasureType]>(
-	[`Вкусная колбаска "До пюрешки"`,              500, 72.00, uType.vendorType_packed, uType.measureType_gramm],
-	[`Невкусная шинка "Кузьмич" (Колбасный Ряд)`,  500, 48.60, uType.vendorType_packed, uType.measureType_gramm],
-	[`Сгущенное молоко "Ичня"`,                    500, 67.50, uType.vendorType_packed, uType.measureType_milliliter],
-	[`Хлеб "Пшеничный", нарезной (Кулиничи)`,      650, 21.40, uType.vendorType_packed, uType.measureType_gramm],
-	[`Хлеб "Пушкаревский", нарезной (Кулиничи)`,   600, 21.40, uType.vendorType_packed, uType.measureType_gramm],
-	[`Майонез "Домашний" (Торчин)`,                580, 39.60, uType.vendorType_packed, uType.measureType_gramm],
-	[`Кетчуп "Лагидный" (Торчин)`,                 450, 18.50, uType.vendorType_packed, uType.measureType_gramm],
-	[`Витамины "Ундевит" упаковка 50 драже`,       1,   22.60, uType.vendorType_unit,   uType.measureType_unit],
-	[`Таблетки "Парацетомол 500" упаковка 10 шт.`, 1,   26.40, uType.vendorType_unit,   uType.measureType_unit],
-	[`Хлеб "Белорусский", нарезной (Киев Хлиб)`,   700, 25.30, uType.vendorType_packed, uType.measureType_gramm],
-	[`Картофель, сорт 2`,                            1,   7.60, uType.vendorType_weighed, uType.measureType_kilogramm],
-	[`Морковь, сорт 1`,                              1,   7.60, uType.vendorType_weighed, uType.measureType_kilogramm],
-	[`Свекла, сорт 1`,                               1,   8.40, uType.vendorType_weighed, uType.measureType_kilogramm],
-	[`оплата интернета`,                             1, 199.00, uType.vendorType_unit,    uType.measureType_unit],
-	[`оплата электроэнергии`,                        1,  1.44, uType.vendorType_weighed, uType.measureType_kilowatt],
-	[`Горох сухой, половинками (Розумный выбор)`,    1, 16.60, uType.vendorType_packed, uType.measureType_kilogramm],
-	[`Гречка (Розумный выбор)`,                      1, 39.90, uType.vendorType_packed, uType.measureType_kilogramm],
-	[`Колбаса "Салями Премиум", варено-копченая (Добров)`, 320, 77.70, uType.vendorType_packed, uType.measureType_gramm],
-	[`Соль пищевая.`,                                 1.5, 7.70, uType.vendorType_packed, uType.measureType_kilogramm]
-).map(item => new ProductUnit(...item)));
+// получение данных из удаленной базы данных.
+
+//TODO: так как создание объектов Product зависимо от типов, необходимо использовать цепочку промисов.
+
+const typesOfMeasure = fetch("http://localhost:8000/api/type/measure")
+	.then(response => response.ok ? response.json() : null)
+	.then(measures => {
+		// числовые ключи соответствуют id типа в БД и обеспечат 
+		// быстрый доступ к типам при сооздании Product.
+		const types = new Map<number, uType.MeasureType>();
+		for (const measure of measures) {
+			// проверка что тип из базы идентичен типу реализованному на клиенте.
+			var type = uType.MeasureType.info(measure.type_name)
+			if (type !== null 
+				&& type.labelFull == measure.label_full
+				&& type.labelShort == measure.label_short
+				&& type.description == (measure.description ?? "")) {
+				types.set(measure.id, type);
+			} else {
+				throw new Error("Несовпадения типов БД/Клиент" + measure.type_name);
+			}
+		}
+		return types;
+	});
+
+const typesOfVendors = fetch("http://localhost:8000/api/type/package")
+	.then(response => response.ok ? response.json() : null)
+	.then(vendors => {
+		// числовые ключи соответствуют id типа в БД и обеспечат 
+		// быстрый доступ к типам при сооздании Product.
+		const types = new Map<number, uType.VendorType>();
+		for (const vendor of vendors) {
+			// проверка что тип из базы идентичен типу реализованному на клиенте.
+			var type = uType.VendorType.info(vendor.type_name)
+			if (type !== null 
+				&& type.labelFull == vendor.label_full
+				&& type.labelShort == vendor.label_short
+				&& type.description == (vendor.description ?? "") ) {
+				types.set(vendor.id, type);
+			} else {
+				console.log(type);
+				console.log(vendor);
+				throw new Error("Несовпадения типов БД/Клиент" + vendor.type_name);
+			}
+		}
+		return types;
+	});
+
+const constDataSet = Promise.all([typesOfMeasure, typesOfVendors])
+	.then(typesMV => {
+		return fetch("http://localhost:8000/api/data/product")
+			.then(response => response.ok ? response.json() : null)
+			.then(products => {
+				const [measures, vendors] = typesMV;
+				const productObjects = new Array<ProductUnit>();
+				for (const p of products) {
+					let measureType = measures.get(p.measure_id);
+					let vendorType = vendors.get(p.package_id);
+					if (measureType && vendorType) {
+						productObjects.push(
+							new ProductUnit(p.title, p.amount, parseFloat(p.price), vendorType, measureType)
+						);
+					} else {
+						throw new Error("Не удалось создать продукт из БД" + p.title);
+					}
+				}
+				return productObjects;
+			});
+	});
+// жуткая хрень эти цепочки промисов....
