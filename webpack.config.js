5ac2d32273ab7e5a25b5cca163061709b1f568e0
Serhii Melnykov <serhii.melnykov@outlook.com>
2022-09-23 00:34:37 +0300

    Переделка систем сборки и компиляции.

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	entry: './src/dist/program.js',
+	output: {
+		filename: 'program.js',
+		path: 'D:\\Projects\\Рационализатор\\Rationalizer\\app\\static\\js'
+	},
+	target: 'node',
+	mode: 'development'
+}
