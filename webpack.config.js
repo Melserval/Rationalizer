2f415a66d8db9a77919a4c1b38ef228080e41232
Serhii Melnykov <serhii.melnykov@outlook.com>
2022-11-18 18:32:36 +0200

    смена системы сборки

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-	entry: './src/dist/program.js',
+	mode: 'development',
+	devtool: "inline-source-map",
+	entry: './src/typescript/program.ts',
 	output: {
 		filename: 'program.js',
 		path: 'D:\\Projects\\Рационализатор\\Rationalizer\\app\\static\\js'
 	},
-	target: 'node',
-	mode: 'development'
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: 'ts-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	}
 }
