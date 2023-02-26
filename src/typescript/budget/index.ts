5e12d007d2fdc08601181aa77cbe97db7a172eb3
Serhii Melnykov <serhii.melnykov@outlook.com>
2023-02-26 03:21:42 +0200

    [client] переработка логики budget/index.ts

--- src/typescript/budget/index.ts
+++ src/typescript/budget/index.ts
@@ -1,35 +1,32 @@
 // иннициализация компонентов модуля.
-import { EventPeriod, periodController }    from "./controller-period-purshase";
+import { EventPeriodController, periodController }    from "./controller-budget-period";
 import  periodFormClbcSet                   from "./form-budget-controller";
 import { getBudgetPeriodLast, addBudgetPeriod } from "../datastorage";
 import { viewBudgetInfoController }         from "./view-budget-info-display";
-import { EventBudget } from "./BudgetPeriod";
+import { BudgetPeriod, EventBudget } from "./BudgetPeriod";
 
 
 
 
 // Создание периода в формой, помещение в контроллер и БД.
 periodFormClbcSet(budget => {
-	addBudgetPeriod(budget);
-	periodController.addPeriod(budget, true);
+	addBudgetPeriod(budget)
+	.then(result => periodController.addPeriod(budget, true))
+	.catch(console.error);
 });
 
 // получение последнего (текщего) периода из бд.
 getBudgetPeriodLast()
 	.then(budget => periodController.addPeriod(budget, true), console.log);
 
+function setDisplayInfo(period: BudgetPeriod) {
+	viewBudgetInfoController.freeMeans    = period.getAmount();
+	viewBudgetInfoController.reserveMeans = period.getReserve();
+	viewBudgetInfoController.utilizeMeans = period.getUtilize();
+}
 
 // обновление данных на дисплее.
-periodController.on(EventPeriod.add, period => {
-	viewBudgetInfoController.freeMeans    = period.getAmount();
-	viewBudgetInfoController.reserveMeans = period.getReserve();
-	viewBudgetInfoController.utilizeMeans = period.getUtilize();
-});
+periodController.on(EventPeriodController.activate, setDisplayInfo);
+periodController.onItem(EventBudget.change, setDisplayInfo);
 
-periodController.onItem(EventBudget.change, period => {
-	viewBudgetInfoController.freeMeans    = period.getAmount();
-	viewBudgetInfoController.reserveMeans = period.getReserve();
-	viewBudgetInfoController.utilizeMeans = period.getUtilize();
-});
-
-export { periodController, EventPeriod };
+export { periodController, EventPeriodController as EventPeriod };
